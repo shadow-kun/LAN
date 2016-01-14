24355efb9d4b63fd764c34d26d4b979e536c902e
Shadow-Kun <shadowreaper@shadowreaper.net>
2016-01-14 14:32:06 +1100

    Feature Fix - I9 Test 1
    Removed ability to create a duplicate entry for an alias

--- com_events/admin/models/store.php
+++ com_events/admin/models/store.php
@@ -465,6 +465,19 @@
 				$table->alias = JApplication::stringURLSafe($table->title);
 			}
 
+			// Checks for duplicate aliases			
+			if($this->aliasDuplicateCheck($table->alias , $table->id) === true)
+			{
+				// If there is a duplicate, then add a number to the end of the alias and retry until that alias is free.
+				$duplicateID = 1;
+			
+				while ($this->aliasDuplicateCheck($table->alias . '-' . $duplicateID, $table->id) === true)
+				{
+					$duplicateID++;
+				}
+				$table->alias = $table->alias . '-' . $duplicateID;
+			}
+			
 			if (empty($table->id)) 
 			{
 				// For a new record.
@@ -517,6 +530,50 @@
 
 				// Put array back together, comma delimited.
 				$this->metakey = implode(', ', $newKeys);
+			}
+		}
+		
+		/**
+		* Verify if the alias is a duplicate.
+		*
+		* @return 		Boolean
+		* @since		0.2.2
+		*/
+		
+		protected function aliasDuplicateCheck($alias, $id)
+		{
+			$db		= $this->getDbo();
+			$query	= $db->getQuery(true);
+			
+			// Select the required fields from the table.
+			$query->select('e.id AS id');
+			$query->from('#__events_shop_stores AS e');
+						
+			// Selects the store that is required.
+			$query->where('e.alias = ' . $db->quote($alias));
+			$result = $db->setQuery($query)->loadObjectList();
+			$db->query();
+			
+			// verifies number of results and returns a result appropriate.
+			if ($db->getNumRows() == 0)
+			{
+				return false;
+			}
+			if ($db->getNumRows() == 1)
+			{
+				// Needs to be verified that the result is the current item.
+				if($id == $result[0]->id)
+				{
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+			else
+			{
+				return true;
 			}
 		}
 	}
